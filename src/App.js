2abe3d2415e5a8e3352d2ea26f061e90710352e6
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-11 12:09:40 -0800

    add Hello Dojo and Things to do

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+////////////////////////////////////////////////////////////////
+//  APPLICATION JAVASCRIPT
+////////////////////////////////////////////////////////////////
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Hello Dojo</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a Marsthon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </main>
     </div>
   );
 }
